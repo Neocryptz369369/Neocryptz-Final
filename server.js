e6b82127a6203de499c2032885ceafebea3d03e0
Neocryptz369369 <neocryptz@yahoo.com>
2026-06-28 01:10:49 -0400

    Remove app.listen and export app for Vercel deployment

--- server.js
+++ server.js
@@ -10,8 +10,5 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.static(__dirname));
 app.get('*', (_req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Neocryptz AI on port', PORT));
-
 // Export for Vercel
 module.exports = app;
